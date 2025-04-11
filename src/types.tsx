8eb757b6892da8eac07b73dd0d2f09025df672a4
trubicynaite <trubicynaite@gmail.com>
2025-04-11 14:36:28 +0300

    added userscontext.tsx and types.tsx

--- src/types.tsx
+++ src/types.tsx
@@ -6,4 +6,20 @@ export type User = {
     passwordText: string,
     dob: string,
     role: "admin" | "user"
-}
+};
+
+export type ChildrenProp = {
+    children: React.ReactElement
+};
+
+export type UsersReducerActionTypes =
+    { type: 'setData', data: User[] } |
+    { type: 'addUser', newUser: User };
+
+export type UsersContextTypes = {
+    users: User[],
+    loggedInUser: User | null,
+    setLoggedInUser: React.Dispatch<React.SetStateAction<User | null>>,
+    dispatch: React.ActionDispatch<[action: UsersReducerActionTypes]>
+};
+
