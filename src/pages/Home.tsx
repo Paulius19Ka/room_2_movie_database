966e43294a7fc27d8f10419939e8c20ffb8ac439
SesLukas <lukas.seskauskas@gmail.com>
2025-04-14 15:57:06 +0300

    Skeleton done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import { useContext } from "react";
+
 import styled from "styled-components";
 
-import MovieCard from "../UI/molecules/MovieCard";
+import { useContext } from "react";
 import MoviesContext from "../contexts/MoviesContext";
-import { MovieContextTypes } from "../types";
+import MovieCard from "../UI/molecules/MovieCard";
 
+import { Container, Skeleton, Box } from "@mui/material";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const StyledDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -14,28 +16,50 @@ const StyledDiv = styled.div`
 `;
 
 const Home = () => {
+  const moviesContext = useContext(MoviesContext);
 
-  const { movies } = useContext(MoviesContext) as MovieContextTypes;
+  if (!moviesContext) return <p>Loading...</p>;
 
-  // logged in user use context here
+  const { movies } = moviesContext;
+  const isLoading = !movies || movies.length === 0;
 
   return (
-    <section>
-      <h2>Home</h2>
-      <StyledDiv>
-        {
-          movies ?
-          movies.map(movie => 
-            <MovieCard
-              data={movie}
-              key={movie.id}
-            />
-          ) :
-          <p>SKELETON HERE!!!!!!!!!</p>
-        }
-      </StyledDiv>
-    </section>
+    <Container sx={{ paddingTop: 4 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 2,
+          justifyContent: "center",
+        }}
+      >
+        {isLoading
+          ? [...Array(6)].map((_, i) => (
+              <Box
+                key={i}
+                sx={{
+                  width: 250,
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 1,
+                }}
+              >
+                <Skeleton
+              variant="rectangular"
+              width="100%"
+              height={360}
+              animation="wave"
+              sx={{ bgcolor: "#e0e0e0" }}
+                />
+          <Skeleton width="80%" sx={{ bgcolor: "#e0e0e0" }} />
+          <Skeleton width="60%" sx={{ bgcolor: "#e0e0e0" }} />
+              </Box>
+            ))
+          : movies.map((movie) => (
+              <MovieCard key={movie.id} data={movie} />
+            ))}
+      </Box>
+    </Container>
   );
-}
- 
+};
 export default Home;
