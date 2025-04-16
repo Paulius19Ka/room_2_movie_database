ea060d3f8ef229f2e368f27ccda8c4c92b1ac96a
Paulius19Ka <kallabow2@gmail.com>
2025-04-16 15:49:44 +0300

    fixed theme context

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -5,20 +5,19 @@ const ThemeContext = createContext<ThemeContextTypes | undefined>(undefined);
 const ThemeProvider = ({ children }: ChildrenProp) => {
 
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
-
-  useEffect(() => {
-    localStorage.setItem('theme', theme);
-  }, [theme]);
-  
   
   const themeToggle = () => {
-    setTheme(prev => prev === 'dark' ? 'light' : 'dark');
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  }
 
+  useEffect(() => {
     document.documentElement.style.setProperty('--text-main', theme === 'dark' ? '#f3f3f3' : '#080808');
     document.documentElement.style.setProperty('--text-secondary', theme === 'dark' ? '#dcdcdc' : '#373737');
     document.documentElement.style.setProperty('--background-main', theme === 'dark' ? '#242424' : '#f3f3f3');
     document.documentElement.style.setProperty('--background-secondary', theme === 'dark' ? '#131313' : '#ffffff');
-  }
+  }, [theme]);
 
   return (
     <ThemeContext.Provider
--- src/outlets/MainOutlet.tsx
+++ src/outlets/MainOutlet.tsx
@@ -3,9 +3,6 @@ import styled from "styled-components";
 
 import Footer from "../../src/UI/organisms/Footer";
 import Header from "../UI/organisms/Header";
-import { useContext } from "react";
-import { ThemeContextTypes } from "../types";
-import ThemeContext from "../contexts/ThemeContext";
 
 const StyledMain = styled.main`
   min-height: calc(100vh - 314.34px - 128px);
@@ -21,12 +18,10 @@ const StyledMain = styled.main`
 
 const MainOutlet = () => {
 
-  const { theme } = useContext(ThemeContext) as ThemeContextTypes;
-
   return (
     <>
       <Header />
-      <StyledMain className={theme === 'dark' ? 'themeDark' : 'themeLight'}>
+      <StyledMain>
         <Outlet />
       </StyledMain>
       <Footer />
