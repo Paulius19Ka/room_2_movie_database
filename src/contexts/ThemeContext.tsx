edfc167797eb509b434978a54c4fecd7de373f28
Paulius19Ka <kallabow2@gmail.com>
2025-04-16 15:24:10 +0300

    finished  theme context

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -13,6 +13,11 @@ const ThemeProvider = ({ children }: ChildrenProp) => {
   
   const themeToggle = () => {
     setTheme(prev => prev === 'dark' ? 'light' : 'dark');
+
+    document.documentElement.style.setProperty('--text-main', theme === 'dark' ? '#f3f3f3' : '#080808');
+    document.documentElement.style.setProperty('--text-secondary', theme === 'dark' ? '#dcdcdc' : '#373737');
+    document.documentElement.style.setProperty('--background-main', theme === 'dark' ? '#242424' : '#f3f3f3');
+    document.documentElement.style.setProperty('--background-secondary', theme === 'dark' ? '#131313' : '#ffffff');
   }
 
   return (
