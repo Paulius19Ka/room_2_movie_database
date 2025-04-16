206e9ece22e159bb22a0edf8dfba94afd60a31e9
Paulius19Ka <kallabow2@gmail.com>
2025-04-16 08:36:26 +0300

    added margins

--- src/outlets/MainOutlet.tsx
+++ src/outlets/MainOutlet.tsx
@@ -7,8 +7,12 @@ import Header from "../UI/organisms/Header";
 const StyledMain = styled.main`
   min-height: calc(100vh - 314.34px - 128px);
 
+  margin: 10px 20px;
+
   @media (min-width: 1024px) {
     min-height: calc(100vh - 335.95px - 128px);
+
+    margin: 10px 40px;
   }
 `;
 
